14cae34f8c43c061c6118f5f44544cb02d7a849c
Ofof22 <79909378+Ofof22@users.noreply.github.com>
2024-01-25 22:40:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/views', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
