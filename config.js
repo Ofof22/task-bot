fd6307aebc2bb2322bb70940e325d297ec89b4ba
Ofof22 <79909378+Ofof22@users.noreply.github.com>
2024-01-25 22:27:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   bot: {
     token:
-      "MTE4MDE4NTA1OTM3MDczNzcwNg.G0bD_5.zx_FiNNZMCmt2PRYndqXLnJEoRUjL3nn7-aM_M",
+      "MTIwMDE1OTc3NDE3NjcxODg2OQ.G1ezvM.QI-6dU_LH7792w0172EOyBuaArRPKLfPQrTXzY",
   },
 };
